c083e65b8527db63f836a219258bab04bbf10c25
JKirkby95 <>
2023-06-23 16:31:45 +0000

    add basic html css and javScript for home page

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// Get the necessary DOM elements
+const homeScreen = document.getElementById('home-screen');
+const startButton = document.getElementById('start-button');
+const gameScreen = document.getElementById('game-screen');
+
+// Add event listener to the start button
+startButton.addEventListener('click', startGame);
+
+// Function to start the game
+function startGame() {
+  // Hide the home screen
+  homeScreen.style.display = 'none';
+  
+  // Display the game screen
+  gameScreen.style.display = 'block';
+  
+}
